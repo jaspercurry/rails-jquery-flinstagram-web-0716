372dbdd0ae9f764ad1310e81988f17bcdbc9365c
jaspercurry <jaspercurry@gmail.com>
2016-09-03 15:30:57 -0400

    made lab

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,1 +1,17 @@
 // Add your JavaScript here
+
+
+$(function(){
+  addFormEventHandler();
+});
+
+
+function addFormEventHandler() {
+$('.ajax').on('ajax:complete', function(e, request, status){
+  var class1 = request.responseJSON.class
+  var id = request.responseJSON.id
+  var message = request.responseJSON.content
+$(`#photo${id}`).attr( "class", `${class1}`)
+$(`div#photo${id}`).html(` ${message}`)
+})
+}
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,9 +5,13 @@ class PicturesController < ApplicationController
   end
 
   def like
+
     @picture = Picture.find(params[:id])
     @picture.update_likes(current_user)
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { render json: { id: @picture.id, class: @picture.heart_class(current_user), content: @picture.likes_message(current_user)}}
+    end
   end
 
 end
